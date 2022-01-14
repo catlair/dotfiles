5bcb1ec9406c03e7bc4743084dec21213756d873
OceanJiang <catlair@qq.com>
2022-01-14 11:13:23 +0800

    Update github-cdn.sh

--- github-cdn.sh
+++ github-cdn.sh
@@ -1,6 +1,7 @@
 sed -i "s/raw\.githubusercontent\.com\/ohmyzsh\/ohmyzsh\/master/cdn\.jsdelivr\.net\/gh\/ohmyzsh\/ohmyzsh/g" ~/.zinit/bin/zinit.zsh
-sed -i "s/raw\.githubusercontent\.com\/sorin-ionescu\/prezto\/master/cdn\.jsdelivr\.net\/gh\/sorin-ionescu\/prezto/g" ~/.zinit/bin/zinit.zsh
+sed -i "s/raw\.githubusercontent\.com\/sorin-ionescu\/prezto\/master/ghproxy\.com\/https:\/\/github\.com\/sorin-ionescu\/prezto/g" ~/.zinit/bin/zinit.zsh
 sed -i "s/github\.com\/ohmyzsh\/ohmyzsh\/trunk/cdn\.jsdelivr\.net\/gh\/ohmyzsh\/ohmyzsh/g" ~/.zinit/bin/zinit.zsh
 sed -i "s/github\.com\/sorin-ionescu\/prezto\/trunk/cdn\.jsdelivr\.net\/gh\/sorin-ionescu\/prezto/g" ~/.zinit/bin/zinit.zsh
-sed -i "s/github\.com/github\.91chifun\.workers\.dev\/https:\/\/github\.com/g" ~/.zinit/bin/zinit-install.zsh
+sed -i "s/github\.com/ghproxy\.com\/https:\/\/github\.com/g" ~/.zinit/bin/zinit-install.zsh
 
+# https:\/\/ghproxy\.com\/https:\/\/raw\.githubusercontent\.com\/
